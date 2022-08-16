0b5ba40a742f5c955782f794438674e9e45b0e94
Jude Okagu <juud0010@gmail.com>
2022-08-16 19:35:54 -0400

    fixxx

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 
 // mongoose.connect('mongodb://127.0.0.1:27017/travel')
-// mongoose.connect('mongodb://127.0.0.1:27017/travel')
+
 const DATABASE = 'mongodb://127.0.0.1:27017/fullstack'
 mongoose
     .connect(DATABASE, {
